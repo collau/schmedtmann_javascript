349ef2474a92edda70192170d8c796cf3e374b98
Junyi <collau@hotmail.com>
2020-04-16 22:37:52 +0800

    Variables and Data Types

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -1,1 +1,17 @@
-console.log('Hello World again!');
+// Variables and Data Types: Number, String, Boolean, Undefined, Null
+// Variable cannot start with numbers of symbols (except for $ or _)
+/*
+var firstName = 'Kevin';
+console.log(firstName);
+
+var lastName = 'De Bruyne';
+var age = 26;
+var fullAge = true;
+console.log(fullAge);
+
+var job; //undefined variable
+console.log(job);
+*/
+
+// Log text into console
+// console.log('Hello World again!');
