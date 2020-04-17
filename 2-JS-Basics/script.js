ce25ab4997ae448bc5189d1f41aada25c8491234
Junyi <collau@hotmail.com>
2020-04-18 00:15:53 +0800

    Basic Operators and Operator Precedence

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -47,3 +47,63 @@ alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + is
 var lastName = prompt('What is his last name?');
 console.log(firstName + ' ' + lastName);
 */
+
+/******************************************************************
+* Basic Operators
+*/
+
+/*
+// Math Operators
+var year, yearTrent, yearVirgil;
+now = 2019;
+ageTrent = 21;
+ageVirgil = 28;
+
+yearTrent = now - ageTrent;
+yearVirgil = now - ageVirgil;
+
+console.log(yearTrent);
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+// Logical Operators
+var trentOlder = ageTrent < ageVirgil;
+console.log(trentOlder);
+
+// typeof operator
+console.log(typeof trentOlder);
+console.log(typeof ageTrent);
+console.log(typeof 'Mark is older than John');
+*/
+
+/******************************************************************
+* Operator Precedence
+*/
+
+var now = 2019;
+var yearTrent = 1998;
+var fullAge = 21;
+
+// Multiple operators
+var isFullAge = now - yearTrent >= fullAge; // true
+console.log(isFullAge);
+
+// Grouping
+var ageTrent = now - yearTrent;
+var ageVirgil = 28;
+var average = (ageTrent + ageVirgil) / 2;
+console.log(average);
+
+// Multiple assignments - Assignment operators works from right-to-left
+var x, y;
+x = y = (3 + 5) * 4 - 6; //26
+console.log(x, y); //26 26
+
+// More operators
+x *= 2; // x = x * 2;
+console.log(x);
+x += 10; // x = x + 10;
+console.log(x);
+x++; // x += 1 // x = x + 1
+console.log(x);
