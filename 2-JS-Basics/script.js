93fc1f7cc2cd378701c0a2267d18c76f9e45c5c8
Junyi <collau@hotmail.com>
2020-04-17 23:42:20 +0800

    Variable Mutation and Type Coercion

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -1,5 +1,9 @@
-// Variables and Data Types: Number, String, Boolean, Undefined, Null
+/******************************************************************
+* Variables and Data Types: Number, String, Boolean, Undefined, Null
+*/
+
 // Variable cannot start with numbers of symbols (except for $ or _)
+
 /*
 var firstName = 'Kevin';
 console.log(firstName);
@@ -11,7 +15,35 @@ console.log(fullAge);
 
 var job; //undefined variable
 console.log(job);
-*/
 
 // Log text into console
-// console.log('Hello World again!');
+console.log('Hello World again!');
+*/
+
+/******************************************************************
+* Variable mutation and type coercion 
+*/
+/*
+var firstName = 'Trent';
+var age = 21;
+
+// Type coercion
+console.log(firstName + ' ' + age); //JS auto coerces types from one another as it is needed
+
+var job, isMarried;
+job = 'footballer';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// Variable mutation
+age = 'twenty-one';
+job = 'right-back';
+
+// Pop-up alert with message
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// Pop-up with message and editText (defining variable with user input in UI)
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName);
+*/
