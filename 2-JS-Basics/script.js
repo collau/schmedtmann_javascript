33dcd02612630bd528bc2bc17efa4ab87edb28ee
Junyi <collau@hotmail.com>
2020-04-19 23:46:44 +0800

    Ternary Operator and Switch Statements

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -137,6 +137,7 @@ if (isMarried) {
 */
 // AND (&&), OR (||), NOT (!)
 
+/*
 var firstName = 'John';
 var age = 20;
 
@@ -148,4 +149,55 @@ if (age < 13) {
     console.log(firstName + ' is a young man.')
 } else {
     console.log(firstName + ' is a man.')
+}
+*/
+
+/******************************************************************
+* Ternary Operator and Switch statements
+*/
+
+// Ternary Operator ==> condition ? if block : else block
+var firstName = 'John';
+var age = 21;
+
+age >= 18 ? console.log(firstName + ' drinks beer.') : console.log(firstName + ' drinks juice.');
+
+var drink = age >= 18 ? 'beer' : 'juice';
+console.log(drink);
+
+/* if (age >= 18) {
+    var drink = 'beer';
+} else {
+    var drink = 'juice';
+}*/
+
+// Switch statement
+var job = 'instructor';
+switch (job) {
+    case 'teacher':
+    case 'instructor':
+        console.log(firstName + ' teaches kids how to code.');
+        break;
+    case 'driver':
+        console.log(firstName + ' drives an uber in Lisbon.');
+        break;
+    case 'designer':
+        console.log(firstName + ' designs beautiful websites.');
+        break;
+    default:
+        console.log(firstName + ' does something else.');
+}
+
+switch (true) {
+    case age < 13:
+        console.log(firstName + ' is a boy.');
+        break;
+    case age >= 13 && age < 20:
+        console.log(firstName + ' is a teenager.');
+        break;
+    case age >= 20 && age < 30:
+        console.log(firstName + ' is a young man.');
+        break;
+    default:
+        console.log(firstName + ' is a man.');
 }
