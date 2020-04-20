11e2f8cfd1f47a577b1c33e264e44dcfd0fb600e
Junyi <collau@hotmail.com>
2020-04-20 20:40:22 +0800

    Functions

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -213,6 +213,7 @@ switch (true) {
 // falsy values: undefined, null, 0, '', NaN
 // truthy values: NOT falsy values
 
+/*
 var height;
 height = 23;
 
@@ -228,3 +229,31 @@ if (height || height === 0) {
 if (height == '23') {
     console.log('The == operator does type coercion!');
 }
+*/
+
+/******************************************************************
+* Functions
+*/
+
+function calculateAge(birthYear) {
+    return 2019 - birthYear;
+}
+
+var ageJohn = calculateAge(1990);
+var ageMike = calculateAge(1948);
+var ageJane = calculateAge(1969);
+console.log(ageJohn, ageMike, ageJane);
+
+function yearsUntilRetirement(year, firstName) {
+    var age = calculateAge(year);
+    var retirement = 65 - age;
+    if (retirement > 0) {
+        console.log(firstName + ' retires in ' + retirement + ' years.');
+    } else {
+        console.log(firstName + ' is already retired.');
+    }
+}
+
+yearsUntilRetirement(1990, 'John');
+yearsUntilRetirement(1948, 'Mike');
+yearsUntilRetirement(1969, 'Jane');
