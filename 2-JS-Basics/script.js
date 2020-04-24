3e23b5525d4e010d2431514c3f69d3ce2906647d
Junyi <collau@hotmail.com>
2020-04-25 01:21:48 +0800

    Loops and Iteration

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -365,6 +365,7 @@ console.log(jane);
 * Objects and Methods
 */
 
+/*
 var john = {
     firstName: 'John',
     lastName: 'Smith',
@@ -382,3 +383,64 @@ var john = {
 
 john.calcAge();
 console.log(john);
+*/
+
+/******************************************************************
+* Loops and iteration
+*/
+
+/* For loop
+1. Initial value of counter
+2. Condition that is evaluated before each loop iteration
+3. Counter update after each iteration
+*/
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+// i = 0, 0 < 10 true, log i to console, i++
+// i = 1, 1 < 10 true, log i to console, i++
+// ...
+// i = 9, 9 < 10 true, log i to console, i++
+// i = 10, 10 < 10 FALSE, exit the loop!
+
+/*
+var john = ['John', 'Smith', 1990, 'designer', false];
+for (var i = 0; i < john.length; i++) {
+    console.log(john[i]);
+};
+
+// While loop
+var i = 0;
+while (i < john.length) {
+    console.log(john[i]);
+    i++;
+}
+*/
+
+// continue and break statements
+/*
+continue => quit the current iteration of the loop and continue with the next one
+break => break out of a loop
+*/
+
+var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+
+for (var i = 0; i < john.length; i++) {
+    if (typeof john[i] !== 'string')
+        continue;
+
+    console.log(john[i]);
+};
+
+for (var i = 0; i < john.length; i++) {
+    if (typeof john[i] !== 'string')
+        break;
+
+    console.log(john[i]);
+};
+
+// Looping backwards
+for (var i = john.length - 1; i >= 0; i--) {
+    console.log(john[i]);
+}
