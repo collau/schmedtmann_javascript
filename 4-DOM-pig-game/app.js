57771d4374a5b09f4a53c91baaba78883804e3e0
Junyi <collau@hotmail.com>
2020-05-11 23:51:52 +0800

    Coding Challenge 6 questions

--- 4-DOM-pig-game/app.js
+++ 4-DOM-pig-game/app.js
@@ -140,3 +140,13 @@ function init() {
 // function btn() {
 //     console.log("button clicked");
 // }
+
+
+/* YOUR 3 CHALLENGES
+Change the game to follow these rules:
+
+1. A player loses his ENTIRE score when he rolls two 6s in a row. After that it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable)
+2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript). This is a good opportunity to use google to figure this out :))
+3. Add another dice to the game, so that there are two dices now. The player loses his current score when on of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
+
+*/
